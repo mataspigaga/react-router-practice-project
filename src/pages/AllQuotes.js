c21be56d243ba4587882d6bdbfb4e6108a35359c
Matas Pigaga <mataspigaga@gmail.com>
2021-07-05 23:01:01 -0400

    quote form and not found page / styled all quotes

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,12 @@
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Max", text: "learning react is fun" },
+  { id: "q2", author: "James", text: "react is dope" },
+];
+
 const AllQuotes = () => {
-  return <h1>All Quotes Page</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQuotes;
